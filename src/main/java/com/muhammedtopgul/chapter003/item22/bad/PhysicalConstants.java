ddcb655b94c4037ccfa23995003570a1dba49e6d
muhammed-topgul <muhammedtopgul.29@gmail.com>
2022-01-28 14:19:35 +0300

    Item 22: Use interfaces only to define types. (Classes and Interfaces)

---
+++ src/main/java/com/muhammedtopgul/chapter003/item22/bad/PhysicalConstants.java
@@ -0,0 +1,16 @@
+package com.muhammedtopgul.chapter003.item22.bad;
+
+/**
+ * @author muhammed-topgul
+ * @since 28.01.2022 12:24
+ */
+
+// BAD EXAMPLE, USE ENUM INSTEAD OF THIS
+public interface PhysicalConstants {
+    // Avogadro's number (1/mol)
+    public static final double AVOGADROS_NUMBER = 6.022_140_857e23;
+    // Boltzmann constant (J/K)
+    public static final double BOLTZMANN_CONSTANT = 1.380_648_52e-23;
+    // Mass of the electron (kg)
+    public static final double ELECTRON_MASS = 9.109_383_56e-31;
+}
