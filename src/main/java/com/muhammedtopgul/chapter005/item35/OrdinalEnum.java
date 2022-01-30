3586ce040c711320e2dc85f5e08cfa56737196e2
muhammed-topgul <muhammedtopgul.29@gmail.com>
2022-01-31 00:08:16 +0300

    Item 35: Use instance fields instead of ordinals. (Enums and Annotations)

---
+++ src/main/java/com/muhammedtopgul/chapter005/item35/OrdinalEnum.java
@@ -0,0 +1,17 @@
+package com.muhammedtopgul.chapter005.item35;
+
+/**
+ * @author muhammed-topgul
+ * @since 31.01.2022 00:05
+ */
+
+public enum OrdinalEnum {
+
+    RED, BLACK, ORANGE;
+
+    public static void main(String[] args) {
+        System.out.println(RED.ordinal());
+        System.out.println(BLACK.ordinal());
+        System.out.println(ORANGE.ordinal());
+    }
+}
